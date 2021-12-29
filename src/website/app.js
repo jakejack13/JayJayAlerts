a36a5d999dcf7ede22fa6f74d5b452956e1fbf37
Jacob Kerr <jck268@ugclinux.cs.cornell.edu>
2021-12-29 00:38:44 -0500

    Created skeleton front facing website with socket connection

---
+++ src/website/app.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const express = require('express');
+const path = require('path');
+
+const dbschema = require('../../shared/schema/database-schema');
+const addresses = require('../../shared/schema/addresses');
+const socketio = require('socket.io');
+
+const app = express();
+const frontServer = http.createServer(app);
+/** @type {socketio.Server} */
+// @ts-ignore
+const io = socketio(frontServer);
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views/'));
+app.use('/views', express.static('views'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+
+io.on('connection', function(socket) {
+    socket.emit('client connected');
+
+    socket.on('channel sent', function(channel) {
+        const data = {};
+
+        for (const field of dbschema.FIELDS) {
+            const req = http.request(
+                new URL(dbschema.fieldRequest(field)), (res) => {
+                    res.on('data', (d) => {
+                        data[field] = d;
+                    });
+                },
+            );
+            req.on('error', (error) => {
+                console.error(error);
+            });
+            req.end();
+        }
+
+        const dataString = JSON.stringify(data);
+        socket.emit('data sent', [dataString]);
+    });
+});
+
+frontServer.listen(
+    addresses.CLIENTFRONTPORT, addresses.CLIENTFRONTHOSTNAME, () => {
+        console.log(`* Website running at http://${addresses.CLIENTFRONTHOSTNAME}:${addresses.CLIENTFRONTPORT}/`);
+    },
+);
---
+++ src/website/views/alert.js
@@ -0,0 +1,20 @@
+const socket = io();
+
+socket.on('client connected', () => {
+    console.log('Connected');
+});
+
+socket.on('data sent', (dataString) => {
+    const data = JSON.parse(dataString);
+    console.log(data);
+});
+
+
+/**
+ * Submits channel to server
+ */
+// eslint-disable-next-line
+function submitChannel() {
+    const channel = document.getElementById('channel').value;
+    socket.emit('channel sent', channel);
+};
